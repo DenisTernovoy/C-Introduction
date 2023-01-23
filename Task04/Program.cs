19c1926337ef774cb4e9e84638de0519092c67c2
Denis Ternovoy <denis.ternovoi@mail.ru>
2023-01-23 22:25:42 +0300

    Task04

---
+++ Task04/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите 1-е число:");
+int num_1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите 2-е число:");
+int num_2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите 3-е число:");
+int num_3 = Convert.ToInt32(Console.ReadLine());
+
+int max = num_1;
+
+if (num_2 > max) {
+    max = num_2;
+}
+if (num_3 > max) {
+    max = num_3;
+}
+
+Console.WriteLine($"Максимальное число: {max}");
