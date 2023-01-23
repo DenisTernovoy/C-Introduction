7a6d5a41c33704a989cef012632b309c95d3586e
Denis Ternovoy <denis.ternovoi@mail.ru>
2023-01-23 21:40:17 +0300

    Task01

---
+++ Task01/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите 1-е число:");
+int num_1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите 2-е число:");
+int num_2 = Convert.ToInt32(Console.ReadLine());
+
+if (num_1 == num_2 * num_2)
+{
+    Console.WriteLine("Первое число является квадратом второго.");
+} else {
+    Console.WriteLine("Первое число НЕ является квадратом второго!");
+}
